c3f37a6ce0b0f5c5d4277d08170d6be2bea2af81
Tim te Beek <timtebeek@gmail.com>
2012-01-24 15:21:42 +0000

    Add stub code to call life science grid which works for greeter, but not yet for makeblastdb

---
+++ lsgp_client.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+'''
+Module to talk to the SARA Life Science Grid Portal.
+
+@author: tbeek
+'''
+
+from urllib2 import HTTPError
+import logging
+import os
+import tarfile
+import tempfile
+import time
+import urllib
+import urllib2
+
+URL_JOBS = 'https://ws2.grid.sara.nl/apps/prod/jobstates/'
+URL_APPS = 'https://ws2.grid.sara.nl/apps/prod/applications/'
+
+LSGP_USER = ''
+LSGP_PASS = ''
+
+
+def _load_credentials():
+    """Read Life Science Grid Portal credentials from file and store in global variables."""
+    #Get path to credential file
+    from __init__ import resource_filename
+    lsgp_credentials_file = resource_filename(__name__, 'credentials/lsg-portal.cfg')
+
+    #Parse credential file
+    from ConfigParser import SafeConfigParser
+    parser = SafeConfigParser()
+    parser.read(lsgp_credentials_file)
+    defaults = parser.defaults()
+
+    #Overwrite global values with read values
+    global LSGP_USER, LSGP_PASS
+    LSGP_USER = defaults['username']
+    LSGP_PASS = defaults['password']
+
+_load_credentials()
+
+
+def run_application(application, values):
+    '''Main method.'''
+    # Invoke greeter version 1.0 with the below argument
+    url_greeter = URL_APPS + application
+    url_job = _send_request(url_greeter, values)
+    jobid = url_job.split('/')[-1]
+    logging.debug('Submitted %s run at %s', application, url_job)
+
+    # Wait for job to finish
+    _wait_for_job(jobid)
+    logging.debug('%s finished', jobid)
+
+    # Retrieve job result file & Extract files from .tgz
+    directory = _save_job_result(jobid)
+    logging.debug('%s saved to %s', jobid, directory)
+
+    # Delete job result
+    _send_request(url_job, method='DELETE')
+    logging.debug('%s deleted', jobid)
+
+    return directory
+
+
+def _send_request(url, values=None, method=None):
+    """
+    Send a request to the SARA Life Science Grid Portal, using the provided credentials to login rather than x509.
+    @param url: url to request / submit to
+    @param values: dictionary of data that should be POSTed to the url (optional)
+    @param method: string HTTP method (optional: POST when data is provided, GET otherwise)
+    """
+    #Add HTTP Basic Authentication
+    password_manager = urllib2.HTTPPasswordMgr()
+    password_manager.add_password('Grid Portal', 'ws2.grid.sara.nl', LSGP_USER, LSGP_PASS)
+    auth_handler = urllib2.HTTPBasicAuthHandler(password_manager)
+    #Create opener using our above auth_handler
+    opener = urllib2.build_opener(auth_handler)
+
+    #Encode data and create request
+    headers = {'Accept': 'text/plain'}
+    data = urllib.urlencode(values) if values else None
+    request = urllib2.Request(url=url, headers=headers, data=data)
+    if method:
+        request.get_method = lambda: method
+
+    #Send request over opener and retrieve response
+    try:
+        response = opener.open(request, timeout=10)
+    except HTTPError as e:
+        print e
+        for k in sorted(e.hdrs.keys()):
+            print k, e.hdrs[k]
+        print e.fp.read()
+        raise e
+
+    #Retrieve
+    content = response.read()
+    response.close()
+    return content
+
+
+def _wait_for_job(jobid):
+    """
+    Wait for a given job to either leave the Queued status, or disappear from the job states page completely.  
+    @param jobid: id of the job to wait for
+    """
+    duration = 10
+    while True:
+        #Retrieve state for all jobs, and convert to 
+        jobstates = _send_request(URL_JOBS)
+        jobstates = dict(line.split('\t') for line in jobstates.strip().split('\r\n')[1:])
+        if jobid not in jobstates or jobstates[jobid] != 'Queued':
+            break
+        #Sleep for up to two minutes
+        time.sleep(duration)
+        if duration < 120:
+            duration += 10
+
+
+def _save_job_result(jobid):
+    """
+    Save the job result gzipped tarfile and extract it's contents to a new temporary filename.
+    @param jobid:
+    """
+    #Retrieve the produced gzipped tarfile    
+    content = _send_request(URL_JOBS + jobid)
+    outputfile = '{0}.tgz'.format(jobid)
+    with open(outputfile, mode='wb') as write_handle:
+        write_handle.write(content)
+    tempdir = tempfile.mkdtemp('_' + jobid, 'lsgp_jobid_')
+
+    #Extract all files to tempdir
+    tar = tarfile.open(outputfile)
+    tar.extractall(path=tempdir)
+    tar.close()
+    return tempdir
+
+
+if __name__ == '__main__':
+    DIR = run_application('greeter/1.0', {'name': 'Tim'})
+    print DIR, os.listdir(DIR)
+    with open(os.path.join(DIR, os.listdir(DIR)[-1])) as read_handle:
+        print read_handle.read()
---
+++ reciprocal_blast_lsgp.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""Module for the reciprocal blast step using the SARA Life Science Grid Portal."""
+
+from divergence.lsgp_client import run_application
+import os
+
+__author__ = "Tim te Beek"
+__contact__ = "brs@nbic.nl"
+__copyright__ = "Copyright 2011, Netherlands Bioinformatics Centre"
+__license__ = "MIT"
+
+MAKEBLASTDB = 'makeblastdb/2.2.25'
+BLASTN = 'blastn/2.2.25'
+BLASTP = 'blastp/2.2.25'
+
+
+def _create_blast_database(run_dir, fasta_file, nucleotide=False):
+    """Create blast database"""
+    dbtype = 'nucl' if nucleotide else 'prot'
+    db_name = '{0}_blast_db'.format(dbtype)
+
+    #Upload fasta file to a new database
+    values = {'dbtype': dbtype,
+              'in': open(fasta_file).read(),
+              'input_type': 'fasta',
+              'out': db_name}
+    #FIXME HTTP/1.1 400 Bad Request: Missing required option -in
+    db_dir = run_application(MAKEBLASTDB, values)
+
+    #Print files in directory
+    print db_dir
+    for dirfile in os.listdir(db_dir):
+        dirfile = os.path.join(db_dir, dirfile)
+        print dirfile
+        #with open(dirfile) as read_handle:
+        #    print read_handle.read()
+
+    return db_dir, db_name
+
+
+#TODO Convert
+#def reciprocal_blast(run_dir, good_proteins_fasta, fasta_files):
+#    """Create blast database for good_proteins_fasta, blast all fasta_files against this database & return hits."""
+#    #Create blast database, retrieve path & name
+#    db_dir, db_name = _create_blast_database(run_dir, good_proteins_fasta)
+#
+#    #Blast individual fasta files against the made blast databank, instead of the much larger good_proteins_fasta
+#    pool = Pool()  # Pool size is initialized to multiprocessing.cpu_count(), or 1 if that fails.
+#
+#    #Submit job for each fasta files, and store future result handles
+#    submitted_jobs = [pool.apply_async(_blast_file_against_database, (db_dir, db_name, fasta)) for fasta in fasta_files]
+#    #Retrieve blast result files from future result handles and store in x_vs_all_hits
+#    x_vs_all_hits = (job.get() for job in submitted_jobs)
+#
+#    #Concatenate the individual blast result files into one
+#    allvsall = os.path.join(db_dir, 'all-vs-all.tsv')
+#    concatenate(allvsall, x_vs_all_hits)
+#    return allvsall
+
+
+#TODO Convert
+#def _blast_file_against_database(db_dir, blast_db, fasta_file, nucleotide=False):
+#    """Blast all genes from genomes one and two against all genomes"""
+#    blast_program = BLASTN if nucleotide else BLASTP
+#    assert os.path.exists(blast_program) and os.access(blast_program, os.X_OK), 'Could not find or run ' + blast_program
+#
+#    #Determine output file name
+#    basename = os.path.splitext(os.path.split(fasta_file)[1])[0]
+#    hits_file = os.path.join(db_dir, basename + '-vs-all.tsv')
+#
+#    #Actually run the blast program
+#    command = [blast_program,
+#               '-db', blast_db,
+#               '-query', fasta_file,
+#               '-outfmt', str(6),
+#               '-out', hits_file]
+#    log.info('Executing: %s', ' '.join(command))
+#    check_call(command, cwd=db_dir, stdout=open('/dev/null', mode='w'), stderr=STDOUT)
+#
+#    #Sanity check
+#    assert os.path.isfile(hits_file) and 0 < os.path.getsize(hits_file), hits_file + ' should exist with some content'
+#    return hits_file
