d8485c70881bae665f5356cf80b67107b35441b0
Tim te Beek <timtebeek@gmail.com>
2012-01-25 14:35:59 +0000

    Convert reciprocal blast method to use the lsgp Split _blast_file_against_database body into one part submission, one part retrieval to support parallel runs without using threads or processes

--- reciprocal_blast_lsgp.py
+++ reciprocal_blast_lsgp.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 """Module for the reciprocal blast step using the SARA Life Science Grid Portal."""
 
-from divergence.lsgp_client import run_application, upload_database
+from divergence import concatenate
+from divergence.lsgp_client import run_application, upload_database, submit_application_run, retrieve_run_result, \
+    send_request
 import os
 import shutil
+import tempfile
 
 __author__ = "Tim te Beek"
 __contact__ = "brs@nbic.nl"
@@ -15,14 +18,36 @@ BLASTN = 'blastn/2.2.25'
 BLASTP = 'blastp/2.2.25'
 
 
+def reciprocal_blast(good_proteins_fasta, fasta_files):
+    """Create blast database for good_proteins_fasta, blast all fasta_files against this database & return hits."""
+    # Create blast database, retrieve path & name
+    database_url = _create_blast_database(good_proteins_fasta)
+
+    # Submit job for each fasta files, and store jobid & hits file name tuples
+    jobids_and_hits_filenames = [_submit_blast_run(database_url, fasta_file) for fasta_file in fasta_files]
+
+    # Retrieve all results, which should only take neglishably longer than waiting for the slowest results
+    x_vs_all_hits = [_retrieve_blast_hits(jobid, hits_file) for jobid, hits_file in jobids_and_hits_filenames]
+
+    # Concatenate the individual blast result files into one
+    allvsall = tempfile.mkstemp(suffix='.tsv', prefix='all_vs_all')[1]
+    concatenate(allvsall, x_vs_all_hits)
+
+    # Clean up local and remote
+    for x_vs_all in x_vs_all_hits:
+        os.remove(x_vs_all)
+    send_request(database_url, method='DELETE')
+
+    return allvsall
+
+
 def _create_blast_database(fasta_file, nucleotide=False):
     """Create blast database"""
     dbtype = 'nucl' if nucleotide else 'prot'
     db_name = '{0}_blast_db'.format(dbtype)
 
     #Run MAKEBLASTDB remotely
-    params = {'dbtype': dbtype,
-              'out': db_name}
+    params = {'dbtype': dbtype, 'out': db_name}
     files = {'in-file[]': fasta_file}
     db_dir = run_application(MAKEBLASTDB, params=params, files=files)
 
@@ -37,40 +62,53 @@ def _create_blast_database(fasta_file, nucleotide=False):
 
 
 def _blast_file_against_database(database_url, fasta_file, nucleotide=False):
-    """Blast all genes from genomes one and two against all genomes"""
+    """
+    Blast file against database, and immediately retrieve and return the resulting BLAST hits file.
+    @param database_url:
+    @param fasta_file:
+    @param nucleotide:
+    """
+    jobid, hits_file = _submit_blast_run(database_url, fasta_file, nucleotide)
+    outside_path = _retrieve_blast_hits(jobid, hits_file)
+    return outside_path
+
+
+def _submit_blast_run(database_url, fasta_file, nucleotide=False):
+    """
+    Submit a BLAST run, returning the created jobid and filename for BLAST hits.
+    @param database_url:
+    @param fasta_file:
+    @param nucleotide:
+    """
     blast_app = BLASTN if nucleotide else BLASTP
-
-    #Determine output file name
+    # Determine output file name
     hits_file = os.path.splitext(os.path.split(fasta_file)[1])[0] + '-vs-all.tsv'
-
-    #Run BLAST[P/N] remotely
-    params = {'db[]': database_url,
-              'out': hits_file,
-              'outfmt': 6}
+    # Run BLAST[P/N] remotely
+    params = {'db[]': database_url, 'out': hits_file, 'outfmt': 6}
     files = {'query': fasta_file}
-    db_dir = run_application(blast_app, params=params, files=files)
+    jobid = submit_application_run(blast_app, params=params, files=files)
+    return jobid, hits_file
 
-    #Construct the likely path to the local file
-    local_path = os.path.join(db_dir, hits_file)
-    assert os.path.isfile(local_path) and 0 < os.path.getsize(local_path), local_path + ' should exist with some content'
 
-    return local_path
+def _retrieve_blast_hits(jobid, hits_file):
+    """
+    Wait for job to complete, retrieve hits and clean up temporary files. Return the path to the created hits_file.
+    @param jobid: jobid for which to retrieve the BLAST hits
+    @param hits_file: name of the created BLAST hits file to retrieve
+    """
+    #Wait for job to complete and retrieve results
+    results_dir = retrieve_run_result(jobid)
 
-#TODO Convert
-#def reciprocal_blast(run_dir, good_proteins_fasta, fasta_files):
-#    """Create blast database for good_proteins_fasta, blast all fasta_files against this database & return hits."""
-#    #Create blast database, retrieve path & name
-#    db_dir, db_name = _create_blast_database(run_dir, good_proteins_fasta)
-#
-#    #Blast individual fasta files against the made blast databank, instead of the much larger good_proteins_fasta
-#    pool = Pool()  # Pool size is initialized to multiprocessing.cpu_count(), or 1 if that fails.
-#
-#    #Submit job for each fasta files, and store future result handles
-#    submitted_jobs = [pool.apply_async(_blast_file_against_database, (db_dir, db_name, fasta)) for fasta in fasta_files]
-#    #Retrieve blast result files from future result handles and store in x_vs_all_hits
-#    x_vs_all_hits = (job.get() for job in submitted_jobs)
-#
-#    #Concatenate the individual blast result files into one
-#    allvsall = os.path.join(db_dir, 'all-vs-all.tsv')
-#    concatenate(allvsall, x_vs_all_hits)
-#    return allvsall
+    # Construct the likely path to the local file
+    local_path = os.path.join(results_dir, hits_file)
+    assert os.path.isfile(local_path) and 0 < os.path.getsize(local_path), local_path + ' should exist with content'
+
+    # Move result file out of results_dir
+    name, ext = os.path.splitext(os.path.split(local_path)[1])
+    outside_path = tempfile.mkstemp(suffix=ext, prefix=name)[1]
+    shutil.move(local_path, outside_path)
+
+    # Remove results_dir, as we would lose directory reference otherwise
+    shutil.rmtree(results_dir)
+
+    return outside_path
